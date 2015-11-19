18ca57366f0086d814a7842d1ca2403405cceb16
Vo Xuan Huy <huyvoxuan8489@gmail.com>
2015-11-19 08:22:02 +0700

    update home 19.11

--- source/assets/js/plugins/plugin.js
+++ source/assets/js/plugins/plugin.js
@@ -10,7 +10,7 @@
   var DEFAULTMODULE = function( element, options ) {
     options = options || {};
     this.element = typeof element === 'object' ? element : document.querySelector(element);
-    this.options = {};
+    this.options = options || {};
     this.options.value = !options.value ? null : options.value;
 
     this.init();
@@ -30,7 +30,7 @@
 
     actions : function() {
       var self = this, elm = self.element, opt = self.options;
-      self.method = function (e) {
+      self.method = function () {
       };
       self.otherMethod = function () {
       };
