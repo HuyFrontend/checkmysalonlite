b82d56c192e2b24cbec4fe78bf53c1abc032ee53
Vo Xuan Huy <huyvoxuan8489@gmail.com>
2015-11-20 07:55:48 +0700

    removeEventListenerOrDetachEventMultiEvent

--- source/assets/js/libs/plugins/1-element-prototype-native.js
+++ source/assets/js/libs/plugins/1-element-prototype-native.js
@@ -29,7 +29,7 @@ Element.prototype.toggleClass = function (className) {
 // same fallback
 Element.prototype.addEventListenerOrAttachEvent = function (targetFunction, event, isDefault) {
     if (!this.addEventListener) {
-        this.attachEvent('on' + event, targetFunction);
+        this.attachEvent('on' + event, targetFunction, isDefault);
     }
     else {
         this.addEventListener(event, targetFunction, isDefault);
@@ -41,7 +41,7 @@ window.constructor.prototype.addEventListenerOrAttachEvent = document.constructo
 Element.prototype.addEventListenerOrAttachEventMultiEvent = function (targetFunction, eventList, isDefault) {
     for ( var i = 0, len = eventList.length; i < len; i++ ) {
         if (!this.addEventListener) {
-        this.attachEvent('on' + eventList[i], targetFunction);
+        this.attachEvent('on' + eventList[i], targetFunction, isDefault);
         }
         else {
             this.addEventListener(eventList[i], targetFunction, isDefault);
@@ -50,6 +50,21 @@ Element.prototype.addEventListenerOrAttachEventMultiEvent = function (targetFunc
 
 };
 window.constructor.prototype.addEventListenerOrAttachEventMultiEvent = document.constructor.prototype.addEventListenerOrAttachEventMultiEvent = Element.prototype.addEventListenerOrAttachEventMultiEvent;
+// remove listener
+Element.prototype.removeEventListenerOrDetachEventMultiEvent = function (targetFunction, eventList, isDefault) {
+    // eventList = ['click', 'touchstart', 'mouseenter'...];
+    for ( var i = 0, len = eventList.length; i < len; i++ ) {
+        if (!this.removeEventListener) {
+        this.detachEvent('on' + eventList[i], targetFunction, isDefault);
+        }
+        else {
+            this.addEventListener(eventList[i], targetFunction, isDefault);
+        }
+    }
+
+};
+window.constructor.prototype.removeEventListenerOrDetachEventMultiEvent = document.constructor.prototype.removeEventListenerOrDetachEventMultiEvent = Element.prototype.removeEventListenerOrDetachEventMultiEvent;
+
 // Element closest id, class, attribute
 Element.prototype.closestId = function (value) {
     var element = this.parentNode;
--- source/assets/js/plugins/plugin - Copy.js
+++ source/assets/js/plugins/plugin - Copy.js
@@ -1,69 +1,82 @@
-
 (function(factory){
-  // CommonJS/RequireJS and 'native' compatibility
-  if(typeof module !== 'undefined' && typeof exports === 'object') {
-    // A commonJS/RequireJS environment
-    if(typeof window !== 'undefined') {
-      // Window and document exist, so return the factory's return value.
-      module.exports = factory();
-    } else {
-      // Let the user give the factory a Window and Document.
-      module.exports = factory;
-    }
-  } else {
-    // Assume a traditional browser.
-    window.DEFAULTMODULE = factory();
-  }
+  window.HoverElement = factory();
 })(function(){
 
-  // DEFAULTMODULE DEFINITION
+  // HoverElement DEFINITION
   // ===================
-  var DEFAULTMODULE = function( element, options ) {
+  var HoverElement = function( element, options ) {
     options = options || {};
     this.element = typeof element === 'object' ? element : document.querySelector(element);
-    this.options = {};
-    this.options.value = !options.value ? null : options.value;
+    this.options = typeof options === 'object' ? options : {};
 
     this.init();
   };
 
-  // DEFAULTMODULE METHODS
+  // HoverElement METHODS
   // ================
-  DEFAULTMODULE.prototype = {
+  HoverElement.prototype = {
 
     init : function() {
-      var self = this, elm = self.element, opt = self.options;
+      var self = this, elm = self.element/*, opt = self.options*/;
       this.actions();
 
-      // ie9+
-      // elm.addEventListener('click', self.toggleRate, true);
-      // fix for all
-      elm.addEventListenerOrAttachEvent(self.toggleRate, 'click', true);
+      elm
+        .removeEventListenerOrDetachEventMultiEvent ( self.hover, ['click', 'mouseenter'] )
+        .addEventListenerOrAttachEventMultiEvent ( self.hover, ['click', 'mouseenter'] );
+      
+      elm
+        .removeEventListenerOrDetachEventMultiEvent ( self.offHover, [ 'mouseout' ] )
+        .addEventListenerOrAttachEventMultiEvent ( self.offHover, [ 'mouseout' ] );
     },
 
     actions : function() {
-      var self = this, elm = self.element, opt = self.options;
+      var self = this, /*elm = self.element, */opt = self.options;
 
-      self.method = function (e) {
-
+      self.hover = function (e) {
+        var me = e.target || e.srcElement;
+        var itemHover = me.getAttribute(opt.item) ? me.getAttribute(opt.item) : me.closestAttributeName(opt.item);
+        if(itemHover) {
+          var elementTarget = itemHover.getAttribute(opt.target) ? document.querySelector('[' + itemHover.getAttribute(opt.target) + ']') : '';
+          if(elementTarget) {
+            opt.elementTarget = elementTarget;
+            if(elementTarget.classList) {
+              elementTarget.classList.add(opt.classActive);
+            }
+            else {
+              elementTarget.addClass(opt.classActive);
+            }
+          }
+        }
+      };
+      self.offHover = function () {
+        if(opt.elementTarget) {
+          if ( opt.elementTarget.classList && opt.elementTarget.classList.contains ( opt.classList ) ){
+            opt.elementTarget.classList.remove(opt.classActive);
+          }
+          else if ( opt.elementTarget.hasClass ( opt.classActive ) ) {
+            opt.elementTarget.removeClass ( opt.classActive );
+          }
+        }
       };
       self.otherMethod = function () {
-
       };
     }
   };
 
-  // DEFAULTMODULE DATA API
+  // HoverElement DATA API
   // =================
-  var DEFAULTMODULEs = document.querySelectorAll('[data-toggle="data-default"]');
-  for (var i = 0, len = DEFAULTMODULEs.length; i < len; i++ ) {
-      var element = DEFAULTMODULEs[i],
+  var HoverElements = document.querySelectorAll('[data-active-contact]');
+  for (var i = 0, len = HoverElements.length; i < len; i++ ) {
+      var element = HoverElements[i],
           options = {
-            value : ''
+            value : '',
+            item: 'data-hover',
+            target: 'data-target',
+            elementTarget: '',
+            classActive: 'active',
+            classHide: 'hide'
           };
-    new DEFAULTMODULE(element, options);
+    new HoverElement(element, options);
   }
-
-  return DEFAULTMODULE;
-
+  return HoverElement;
 });
