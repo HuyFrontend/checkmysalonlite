d8826884b83d49667417bec432d9186143b4f005
Huy Vo <huy.vo@sutrixmedia.com>
2015-11-20 09:24:52 +0700

    [update]

--- source/assets/js/libs/plugins/1-element-prototype-native.js
+++ source/assets/js/libs/plugins/1-element-prototype-native.js
@@ -2,113 +2,144 @@ Element.prototype.hasClass = function (className) {
   return new RegExp(' ' + className + ' ').test(' ' + this.className + ' ');
 };
 Element.prototype.addClass = function (className) {
-    if (!this.hasClass(className)) {
-        this.className += ' ' + className;
-    }
+  if (!this.hasClass(className)) {
+    this.className += ' ' + className;
+  }
 };
 Element.prototype.removeClass = function (className) {
-    var newClass = ' ' + this.className.replace(/[\t\r\n]/g, ' ') + ' ';
-    if (this.hasClass(className)) {
-        while (newClass.indexOf( ' ' + className + ' ') >= 0) {
-            newClass = newClass.replace(' ' + className + ' ', ' ');
-        }
-        this.className = newClass.replace(/^\s+|\s+$/g, ' ');
+  var newClass = ' ' + this.className.replace(/[\t\r\n]/g, ' ') + ' ';
+  if (this.hasClass(className)) {
+    while (newClass.indexOf( ' ' + className + ' ') >= 0) {
+      newClass = newClass.replace(' ' + className + ' ', ' ');
     }
+    this.className = newClass.replace(/^\s+|\s+$/g, ' ');
+  }
 };
 Element.prototype.toggleClass = function (className) {
   var newClass = ' ' + this.className.replace(/[\t\r\n]/g, ' ') + ' ';
     if (this.hasClass(className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0) {
-            newClass = newClass.replace(' ' + className + ' ', ' ');
-        }
-        this.className = newClass.replace(/^\s+|\s+$/g, ' ');
-    } else {
-        this.className += ' ' + className;
+      while (newClass.indexOf(' ' + className + ' ') >= 0) {
+        newClass = newClass.replace(' ' + className + ' ', ' ');
+      }
+      this.className = newClass.replace(/^\s+|\s+$/g, ' ');
+    }
+    else {
+      this.className += ' ' + className;
     }
 };
 // same fallback
 Element.prototype.addEventListenerOrAttachEvent = function (targetFunction, event, isDefault) {
-    if (!this.addEventListener) {
-        this.attachEvent('on' + event, targetFunction, isDefault);
+  if (!this.addEventListener) {
+    if(isDefault) {
+      this.attachEvent('on' + event, targetFunction, isDefault);
     }
     else {
-        this.addEventListener(event, targetFunction, isDefault);
+      this.attachEvent('on' + event, targetFunction );
     }
+  }
+  else {
+    if(isDefault) {
+      this.addEventListener(event, targetFunction, isDefault);
+    }
+    else {
+      this.addEventListener(event, targetFunction );
+    }
+  }
 };
 // append prototype to document, window
 window.constructor.prototype.addEventListenerOrAttachEvent = document.constructor.prototype.addEventListenerOrAttachEvent = Element.prototype.addEventListenerOrAttachEvent;
 
 Element.prototype.addEventListenerOrAttachEventMultiEvent = function (targetFunction, eventList, isDefault) {
-    for ( var i = 0, len = eventList.length; i < len; i++ ) {
-        if (!this.addEventListener) {
+  for ( var i = 0, len = eventList.length; i < len; i++ ) {
+    if (!this.addEventListener) {
+      if(isDefault) {
         this.attachEvent('on' + eventList[i], targetFunction, isDefault);
-        }
-        else {
-            this.addEventListener(eventList[i], targetFunction, isDefault);
-        }
+      }
+      else {
+        this.attachEvent('on' + eventList[i], targetFunction);
+      }
     }
-
+    else {
+      if(isDefault) {
+        this.addEventListener(eventList[i], targetFunction, isDefault);
+      }
+      else {
+        this.addEventListener(eventList[i], targetFunction);
+      }
+    }
+  }
 };
 window.constructor.prototype.addEventListenerOrAttachEventMultiEvent = document.constructor.prototype.addEventListenerOrAttachEventMultiEvent = Element.prototype.addEventListenerOrAttachEventMultiEvent;
 // remove listener
 Element.prototype.removeEventListenerOrDetachEventMultiEvent = function (targetFunction, eventList, isDefault) {
-    // eventList = ['click', 'touchstart', 'mouseenter'...];
-    for ( var i = 0, len = eventList.length; i < len; i++ ) {
-        if (!this.removeEventListener) {
-        this.detachEvent('on' + eventList[i], targetFunction, isDefault);
-        }
-        else {
-            this.addEventListener(eventList[i], targetFunction, isDefault);
-        }
-    }
+  // eventList = ['click', 'touchstart', 'mouseenter'...];
+  for ( var i = 0, len = eventList.length; i < len; i++ ) {
+    if (!this.removeEventListener) {
 
+      if(isDefault) {
+        this.detachEvent('on' + eventList[i], targetFunction, isDefault);
+      }
+      else {
+        this.detachEvent('on' + eventList[i], targetFunction);
+      }
+    }
+    else {
+
+      if(isDefault) {
+        this.addEventListener(eventList[i], targetFunction, isDefault);
+      }
+      else {
+        this.addEventListener(eventList[i], targetFunction);
+      }
+    }
+  }
 };
 window.constructor.prototype.removeEventListenerOrDetachEventMultiEvent = document.constructor.prototype.removeEventListenerOrDetachEventMultiEvent = Element.prototype.removeEventListenerOrDetachEventMultiEvent;
 
 // Element closest id, class, attribute
 Element.prototype.closestId = function (value) {
-    var element = this.parentNode;
-    if(!value) {
-        return null;
+  var element = this.parentNode;
+  if(!value) {
+    return null;
+  }
+  while (element.id !== value) {
+    element = element.parentNode;
+    if(!element) {
+      return null;
     }
-    while (element.id !== value) {
-        element = element.parentNode;
-        if(!element) {
-            return null;
-        }
-    }
-    return element;
+  }
+  return element;
 };
 Element.prototype.closestClass = function (value) {
-    var parent = this.parentNode;
-    while (parent != document.body) {
-        if ((parent) && (!!parent.className.match(new RegExp('(\\s|^)' + value + '(\\s|$)')))) {
-            return parent;
-        }
-        else {
-            parent = parent.parentNode;
-        }
+  var parent = this.parentNode;
+  while (parent != document.body) {
+    if ((parent) && (!!parent.className.match(new RegExp('(\\s|^)' + value + '(\\s|$)')))) {
+      return parent;
     }
-    return null;
+    else {
+      parent = parent.parentNode;
+    }
+  }
+  return null;
 };
 Element.prototype.closestAttributeName = function (value) {
-    var parent = this.parentNode;
-    while (parent != document.body) {
-        if ( parent && parent.getAttribute(value) !== null ) {
-            return parent;
-        }
-        else {
-            parent = parent.parentNode;
-        }
+  var parent = this.parentNode;
+  while (parent != document.body) {
+    if ( parent && parent.getAttribute(value) !== null ) {
+      return parent;
     }
-    return null;
+    else {
+      parent = parent.parentNode;
+    }
+  }
+  return null;
 };
 Element.prototype.getCSSValue = function (cssType) {
-    /*cssType: margin, left...*/
-    var cssValue = null;
-    if (this.currentStyle) {
-        cssValue = this.currentStyle[cssType];
-    } else if (window.getComputedStyle) {
-        cssValue = window.getComputedStyle(this, null).getPropertyValue(cssType);
-    }
+  /*cssType: margin, left...*/
+  var cssValue = null;
+  if (this.currentStyle) {
+    cssValue = this.currentStyle[cssType];
+  } else if (window.getComputedStyle) {
+    cssValue = window.getComputedStyle(this, null).getPropertyValue(cssType);
+  }
 };
