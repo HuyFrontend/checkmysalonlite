3d2f048957feabd99c4e428f417da139b91c0ec0
Huy Vo <huy.vo@sutrixmedia.com>
2015-12-22 18:07:09 +0700

    update

--- another/train-angular/service/service.js
+++ another/train-angular/service/service.js
@@ -22,15 +22,73 @@ app.factory('notify', function () {
     }
   };
 });
-// service notify2
-app.factory('notify2', function () {
+
+// service
+// app.factory('customService', function () {
+//   return function () {
+//     alert(123);
+//   };
+// });
+// or service replace to factory
+app.service('customService', function () {
   return function () {
     alert(123);
   };
+
 });
-// controller use notify/notify2
-app.controller('MyController', ['$scope','notify2', function ($scope, notify2) {
+
+app.service('myFactory', function ($http, $q) {
+  var service = {},
+      baseUrl = 'https://itunes.apple.com/search?term=',
+      artist = '',
+      finalUrl = '';
+  var makeUrl = function () {
+    artist = artist.split(' ').join('+');
+    finalUrl = baseUrl + artist + '&callback=JSON_CALLBACK';
+    return finalUrl;
+  };
+  service.setArtist = function(art) {
+    artist = art;
+  };
+  service.getArtist = function (art) {
+    return artist;
+  };
+  service.callItunes = function () {
+    makeUrl();
+    var deferred = $q.defer();
+    $http({
+      method: 'JSONP',
+      url: finalUrl
+    })
+    .success(function (data) {
+        deferred.resolve(data);
+    })
+    .error(function (){
+      deferred.reject('an error');
+    });
+    return deferred.promise;
+  };
+  return service;
+});
+
+// controller use notify/customService
+app.controller('MyController', ['$scope','customService', function ($scope, customService) {
   $scope.callNotify = function(msg) {
-    notify2();
+    customService();
   };
 }]);
+
+app.controller('myFactoryCtrl', function ($scope, myFactory) {
+  $scope.data = {};
+  $scope.updateArtist = function () {
+    myFactory.setArtist($scope.data.artist);
+  };
+  $scope.submitArtist = function () {
+    myFactory.callItunes()
+    .then(function (data) {
+      $scope.data.artistData = data;
+    }, function (data) {
+      alert(data);
+    });
+  };
+});
